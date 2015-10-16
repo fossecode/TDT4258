e931b5d7cd0227c3cc397f9d0ddcfed6f1a9438c
Michael McMillan <email@michaelmcmillan.net>
2015-10-16 10:53:05 +0200

    separated concerns into header files

--- ex2/interrupt_handlers.c
+++ ex2/interrupt_handlers.c
@@ -2,28 +2,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <math.h>
-
 #include "efm32gg.h"
 
-#define c 261
-#define d 294
-#define e 329
-#define f 349
-#define g 391
-#define gS 415
-#define a 440
-#define aS 455
-#define b 466
-#define cH 523
-#define cSH 554
-#define dH 587
-#define dSH 622
-#define eH 659
-#define fH 698
-#define fSH 740
-#define gH 784
-#define gSH 830
-#define aH 880
+#include "frequencies.h"
+#include "tune.h"
+#include "songs/imperial_march.h"
 
 int	   count = 0;
 double noise = 0;
