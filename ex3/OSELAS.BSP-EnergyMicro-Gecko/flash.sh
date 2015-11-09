89a3e3fa4c2b79f805d815fade04ace0aa537e6c
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2015-11-09 13:28:50 +0100

    driver connected to software

---
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/flash.sh
@@ -0,0 +1,6 @@
+ptxdist clean driver-gamepad
+ptxdist compile driver-gamepad
+ptxdist targetinstall driver-gamepad
+ptxdist targetinstall kernel
+ptxdist image root.romfs
+ptxdist test flash-rootfs
--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,39 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/ioport.h>
+#include "efm32gg.h"
+
+struct cdev my_cdev;
+dev_t my_dev;
+struct class *cl;
+
+
+/* User program opens the driver */
+static int open_driver(struct inode *inode, struct file *filp);
+
+/* User program closes the driver */
+static int release_driver(struct inode *inode, struct file *filp);
+
+/* User program reads from the driver */
+static int read_driver(struct file *filp, char __user *buff, size_t count, loff_t *offp);
+
+/* User program writes to the the driver */
+static int write_driver(struct file *filp, const char __user *buff, size_t count, loff_t *offp);
+
+static struct file_operations my_fops = {
+	.owner = THIS_MODULE,
+	.read = read_driver,
+	.write = write_driver,
+	.open = open_driver,
+	.release = release_driver
+};
+
+
+
 
 /*
  * template_init - function to insert this module into kernel space
@@ -17,9 +50,46 @@
 
 static int __init template_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
+	
+	alloc_chrdev_region(&my_dev ,0 , 1, "driver-gamepad");
+
+	cdev_init(&my_cdev, &my_fops);
+
+	cl = class_create(THIS_MODULE, "driver-gamepad");
+	device_create(cl, NULL, my_dev, NULL, "driver-gamepad");
+	*request_region(GPIO_PA_BASE, 0x120, "driver-gamepad");
+
+
+	cdev_add(&my_cdev, my_dev, 1);
+	printk("Hello world, here is your module speaking\n");
 	return 0;
 }
+
+
+
+/* User program opens the driver */
+static int open_driver(struct inode *inode, struct file *filp){
+	return 0;
+}
+
+/* User program closes the driver */
+static int release_driver(struct inode *inode, struct file *filp){
+	return 0;
+}
+
+/* User program reads from the driver */
+static int read_driver(struct file *filp, char __user *buff, size_t count, loff_t *offp){
+	printk("yolo");
+	return 0;
+}
+
+/* User program writes to the the driver */
+static int write_driver(struct file *filp, const char __user *buff, size_t count, loff_t *offp){
+	return 0;
+}
+
+
+
 
 /*
  * template_cleanup - function to cleanup this module from kernel space
@@ -30,7 +100,9 @@ static int __init template_init(void)
 
 static void __exit template_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+	unregister_chrdev_region(my_dev ,1);
+	release_region(GPIO_PA_BASE, 0x120);
+	printk("Short life for a small module...\n");
 }
 
 module_init(template_init);
